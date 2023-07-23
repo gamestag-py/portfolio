3461c23c8b2c2cfac33b9feac1211ec3c8f09c0d
Shivansh Agarwal <121762262+gamestag-py@users.noreply.github.com>
2023-07-23 23:18:50 +0530

    Add files via upload

---
+++ email.php
@@ -0,0 +1,9 @@
+<?php
+
+$senderName = 'Shivansh Agarwal You Got An Email From Portfolio';// RESULT NAME HERE 
+
+$sender = 'From:' .$senderName. '<ANY EMAIL OTHER THEN RECEIVER EMAIL ADDRESS>';// SEDER MAIL HERE
+
+$recvEmail = 'YOUR EMAIL ADDRESS'; // RECEIVER MAIL HERE 
+
+?>
